40751fd23e0f598d1c11e78ca67aca7d0b3ad684
hemalathabai <110841116+hemalathabai@users.noreply.github.com>
2023-05-06 14:12:59 +0530

    1 file modified

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ let weather = 'hot';
 if (weather === 'hot') {
   console.log('Weather is hot');
 } else console.log('Weather is Sunny');
+
+let grade = 'A';
+
+switch (grade) {
+  case 'S':
+    console.log('Super Grade!');
+    break;
+  case 'A':
+  case 'B':
+    console.log('Excellent Grade!');
+    break;
+  case 'E':
+    console.log('Just Pass!');
+    break;
+  case 'U':
+    console.log('Failed Grade!');
+    break;
+  default:
+    console.log('Unkown Grade');
+}
