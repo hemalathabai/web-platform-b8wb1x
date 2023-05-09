9447407b284e53d145f8ffdaf570cc6dd84982f6
hemalathabai <110841116+hemalathabai@users.noreply.github.com>
2023-05-10 01:05:00 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
-console.log('Hello!');
-console.log('Hello World');
+// console.log('Hello!');
+// console.log('Hello World');
 
-let hour = 11;
+// let hour = 11;
 
-if (hour >= 0 && hour <= 13) {
-  console.log('Good Morning');
-} else if (hour >= 13 && hour <= 17) {
-  console.log('Good Afternoon');
-} else console.log('Good Evening');
+// if (hour >= 0 && hour <= 13) {
+//   console.log('Good Morning');
+// } else if (hour >= 13 && hour <= 17) {
+//   console.log('Good Afternoon');
+// } else console.log('Good Evening');
 
-let weather = 'hot';
+// let weather = 'hot';
 
-if (weather === 'hot') {
-  console.log('Weather is hot');
-} else console.log('Weather is Sunny');
+// if (weather === 'hot') {
+//   console.log('Weather is hot');
+// } else console.log('Weather is Sunny');
 
-let grade = 'A';
+// let grade = 'A';
 
-switch (grade) {
-  case 'S':
-    console.log('Super Grade!');
-    break;
-  case 'A':
-  case 'B':
-    console.log('Excellent Grade!');
-    break;
-  case 'E':
-    console.log('Just Pass!');
-    break;
-  case 'U':
-    console.log('Failed Grade!');
-    break;
-  default:
-    console.log('Unkown Grade');
-}
+// switch (grade) {
+//   case 'S':
+//     console.log('Super Grade!');
+//     break;
+//   case 'A':
+//   case 'B':
+//     console.log('Excellent Grade!');
+//     break;
+//   case 'E':
+//     console.log('Just Pass!');
+//     break;
+//   case 'U':
+//     console.log('Failed Grade!');
+//     break;
+//   default:
+//     console.log('Unkown Grade');
+// }
 
-let mobile = 70000;
+// let mobile = 70000;
 
-switch (true) {
-  case mobile > 60000:
-    console.log('Buy iPhone Mobile');
-    break;
-  case mobile > 10000:
-    console.log('Buy Android Mobile');
-    break;
-  case mobile > 5000 && mobile < 10000:
-    console.log('Buy basic Mobile');
-    break;
-  default:
-    coneole.log("You can't afford mobile phone now!");
-}
+// switch (true) {
+//   case mobile > 60000:
+//     console.log('Buy iPhone Mobile');
+//     break;
+//   case mobile > 10000:
+//     console.log('Buy Android Mobile');
+//     break;
+//   case mobile > 5000 && mobile < 10000:
+//     console.log('Buy basic Mobile');
+//     break;
+//   default:
+//     coneole.log("You can't afford mobile phone now!");
+// }
 
-let cell = 5000;
+// let cell = 5000;
 
-if (cell > 60000) {
-  console.log('Buy I Phone Mobile!');
-} else if (cell < 10000) {
-  console.log('Buy Android Mobile!');
-} else if (cell > 5000 && cell < 10000) {
-  console.log('Buy Basic Mobile');
-} else {
-  console.log('You can;t afford Mobile phone');
-}
+// if (cell > 60000) {
+//   console.log('Buy I Phone Mobile!');
+// } else if (cell < 10000) {
+//   console.log('Buy Android Mobile!');
+// } else if (cell > 5000 && cell < 10000) {
+//   console.log('Buy Basic Mobile');
+// } else {
+//   console.log('You can;t afford Mobile phone');
+// }
 
-let i = 0;
+let i = 15;
 while (i >= 1) {
   if (i % 2 !== 0) {
     console.log('Odd Number #' + i);
   }
   i--;
+  console.log(i);
 }
 do {
+  console.log(i);
   if (i % 2 !== 0) {
     console.log('Odd Number #' + i);
   }
